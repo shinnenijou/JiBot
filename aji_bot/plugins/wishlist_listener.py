4c6b41c8db3ce3f33c06d3ed4e9398c4fb11fe0d
Shinnenijou <littlesword111@gmail.com>
2022-02-13 16:31:33 +0800

    Update wishlist_listener.py
    调整了监听逻辑，在获取到非正常的html页面时将会二次确认愿望单是否为空

--- aji_bot/plugins/wishlist_listener.py
+++ aji_bot/plugins/wishlist_listener.py
@@ -1,5 +1,6 @@
 import requests
 import nonebot
+import time
 from nonebot.plugin import require
 
 ########## URL #########
@@ -8,19 +9,37 @@ from nonebot.plugin import require
 #url = "https://www.amazon.co.jp/hz/wishlist/ls/1JL7SPMBV2XB2?ref_=wl_fv_le"
 # 咪太君
 name = "mitagun"
-url = "https://www.amazon.co.jp/gp/aw/ls/ref=aw_wl_lol_wl?ie=UTF8&lid=WQJIE8LKY4EB"
+URL = "https://www.amazon.co.jp/gp/aw/ls/ref=aw_wl_lol_wl?ie=UTF8&lid=WQJIE8LKY4EB"
 # test
 #name = "阿鸡"
 #url = "https://www.amazon.jp/hz/wishlist/ls/1BXUQQ5TL2GDO?ref_=wl_share"
 ########################
 
 ########## Var #########
-# 保存上一次请求得到的愿望单物品数据
 global prev_list
+global wish_list
+# 保存上一次请求得到的愿望单物品数据
 prev_list = []
 # 保存本次请求得到的愿望单物品数据
 wish_list = []
+# 用于HTTP请求的请求头
+HEADERS = {}
+HEADERS["Host"] = "www.amazon.co.jp"
+HEADERS["Accept"] = "text/html"
+HEADERS["Accept-Language"] = "ja-JP"
+HEADERS["Connection"] = "close"
 ########################
+
+def request(url, headers):
+    try:
+        # 向amazon请求网页html
+        response = requests.get(url, headers=headers)
+        response.encoding = "UTF-8"
+        resp = response.text
+    except:
+        # 发生任何异常都跳出
+        resp = ""
+    return resp
 
 def find_item(string, begin):
     item_beg = string.find("itemName", begin, len(string))
@@ -33,28 +52,18 @@ def find_item(string, begin):
         item_title = string[title_beg + 7: title_end - 2].strip()
     return item_title, title_end
 
-def check_new_list(url):
-    # 监听给定url是否包含愿望单物品
-    wishlist = []
-    try:
-        # 向amazon请求网页html
-        response = requests.request("GET", url)
-        response.encoding = "UTF-8"
-        resp = response.text
-    except:
-        # 发生任何异常都跳出
-        resp = ""
-
-    if resp:
+def find_items(string):
+    temp_list = []
+    if string:
         # 如果get没有发生异常，则对返回的html进行处理
         begin = 0
         # 将返回数据中包含的愿望单物品全部添加至列表中
         while begin != -1:
             # 查找愿望单中是否有物品
-            item_title, begin = find_item(resp, begin)
+            item_title, begin = find_item(string, begin)
             if item_title:
-                wishlist.append(item_title)
-    return wishlist
+                temp_list.append(item_title)
+    return temp_list
 
 def check_items(list1, list2):
     # 比对两个列表，找出list1中不在list2中的元素
@@ -64,6 +73,20 @@ def check_items(list1, list2):
             not_include.append(item)
     return not_include
 
+def print_items(bot, items, str):
+    if items:
+        msg = f"检测到 {name} 的愿望单以下物品被{str}: \r\n"
+        for item in items:
+            msg += item + "\r\n"
+        try:
+            print(msg)
+            #bot.send_group_msg(group_id = 235976635, message = new_msg)
+        except:
+            pass
+ 
+def check_clear(string):
+    return string.find("このリストにはアイテムはありません") == -1
+
 scheduler = require("nonebot_plugin_apscheduler").scheduler
 # @scheduler.scheduled_job(
 #     "interval",
@@ -72,29 +95,20 @@ scheduler = require("nonebot_plugin_apscheduler").scheduler
 
 async def listen():
     global prev_list
-    wish_list = []
-    new_items = []
-    buyed_items = []
-    bot = nonebot.get_bot()
-    wish_list = check_new_list(url)
-    new_items = check_items(wish_list, prev_list)
-    buyed_items = check_items(prev_list, wish_list)
-    prev_list = wish_list
-    if new_items:
-        new_msg = f"检测到 {name} 的愿望单新添加如下物品: \r\n"
-        for item in new_items:
-            new_msg += item + "\r\n"
-        try:
-            await bot.send_group_msg(group_id = 235976635, message = new_msg)
-        except:
-            pass
-    if buyed_items:
-        buyed_msg = f"检测到 {name} 的愿望单以下物品被清: \r\n"
-        for item in buyed_items:
-            buyed_msg += item + "\r\n"
-        try:
-            await bot.send_group_msg(group_id = 235976635, message = buyed_msg)
-        except:
-            pass
+    global wish_list
+    try:
+        bot = nonebot.get_bot()
+        text = request(URL, HEADERS)
+        wish_list = find_items(text)
+        # 如果愿望单突然完全清空，则检查文本中是否确实包含无物品信息
+        if not wish_list and not check_clear(text):
+            wish_list = prev_list
+        new_items = check_items(wish_list, prev_list)
+        buyed_items = check_items(prev_list, wish_list)
+        prev_list = wish_list
+        print_items(bot, new_items, "添加")
+        print_items(bot, buyed_items, "清除")
+    except:
+        pass
 
 scheduler.add_job(listen, "interval", minutes=10)
