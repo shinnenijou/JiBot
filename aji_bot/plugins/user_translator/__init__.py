6a48034c7085f903fc8a21555b124ca965ffa62f
Shinnenijou <littlesword111@gmail.com>
2022-03-05 13:20:33 +0800

    update translate
    支持在设定翻译用户时指定翻译语种

--- aji_bot/plugins/user_translator/__init__.py
+++ aji_bot/plugins/user_translator/__init__.py
@@ -6,7 +6,7 @@ from tencentcloud.common.profile.client_profile import ClientProfile
 
 import nonebot
 from nonebot import on_message, on_command
-from nonebot.permission import SUPERUSER, USER
+from nonebot.permission import SUPERUSER
 from nonebot.adapters.onebot.v11 import GroupMessageEvent
 
 import json
@@ -14,6 +14,7 @@ import json
 # set Tencent TMT API
 SECRETID = str(nonebot.get_driver().config.dict()["api_secretid"])
 SECRETKEY = str(nonebot.get_driver().config.dict()["api_secretkey"])
+REGION = str(nonebot.get_driver().config.dict()["api_region"])
 cred = credential.Credential(SECRETID, SECRETKEY)
 httpProfile = HttpProfile(endpoint="tmt.ap-tokyo.tencentcloudapi.com")
 clientProfile = ClientProfile(
@@ -21,18 +22,16 @@ clientProfile = ClientProfile(
     language="en-US",
     httpProfile=httpProfile
 )
-client = tmt_client.TmtClient(cred, "ap-seoul", clientProfile)
+client = tmt_client.TmtClient(cred, REGION, clientProfile)
 req = models.TextTranslateRequest()
-req.Source = "ja"
-req.Target = "zh"
 req.ProjectId = 0
 
 # load translate users config
 try:
     with open("./aji_bot/plugins/user_translator/config.ini", "r") as file:
-        TRANSLATE_USERS = tuple(json.loads(file.read()))
+        TRANSLATE_USERS = json.loads(file.read())
 except FileNotFoundError:
-    TRANSLATE_USERS = ()
+    TRANSLATE_USERS = {}
 
 # DEBUG
 matcher = on_command(cmd="已开启",temp=False, priority=1, block=True,
@@ -48,27 +47,33 @@ matcher = on_command(cmd="开启翻译",temp=False, priority=1, block=True,
 @matcher.handle()
 async def add_user(event:GroupMessageEvent):
     global TRANSLATE_USERS
-    cmd = event.get_plaintext().split()[1]
-    if cmd.isdigit():
-        session_id = f"{event.get_session_id().rpartition('_')[0]}_{cmd}"
+    try:
+        user_id = event.get_plaintext().split()[1]
+        setting = event.get_plaintext().split()[2]
+        source = setting.partition("->")[0]
+        target = setting.partition("->")[2]
+        isValidCmd = user_id.isdigit() and source and target
+    except:
+        isValidCmd = False
+    if isValidCmd:
+        session_id = f"{event.get_session_id().rpartition('_')[0]}_{user_id}"
         try:
             file = open("./aji_bot/plugins/user_translator/config.ini", "r")
             config = json.loads(file.read())
         except:
             file = open("./aji_bot/plugins/user_translator/config.ini", "w")
-            config = []
+            config = {}
             file.write(json.dumps(config))
         file.close()
         if session_id not in config:
-            config.append(session_id)
+            config[session_id] = {"source":source, "target":target}
             file = open("./aji_bot/plugins/user_translator/config.ini", "w")
             file.write(json.dumps(config))
             file.close()
-            await matcher.send(f"成功开启 QQ{cmd} 的发言翻译功能")
-            TRANSLATE_USERS = tuple(config)
-            print(TRANSLATE_USERS)
+            await matcher.send(f"成功开启 QQ{user_id} 的发言翻译功能")
+            TRANSLATE_USERS = config
         else:
-            await matcher.send(f"QQ{cmd} 的发言翻译功能正在运行")
+            await matcher.send(f"QQ{user_id} 的发言翻译功能正在运行")
 
 # EVENT: delete translate user
 matcher = on_command(cmd="关闭翻译",temp=False, priority=1, block=True,
@@ -76,27 +81,30 @@ matcher = on_command(cmd="关闭翻译",temp=False, priority=1, block=True,
 @matcher.handle()
 async def del_user(event:GroupMessageEvent):
     global TRANSLATE_USERS
-    cmd = event.get_plaintext().split()[1]
-    if cmd.isdigit():
-        session_id = f"{event.get_session_id().rpartition('_')[0]}_{cmd}"
+    try:
+        user_id = event.get_plaintext().split()[1]
+        isValidCmd = user_id.isdigit()
+    except:
+        isValidCmd = False
+    if isValidCmd:
+        session_id = f"{event.get_session_id().rpartition('_')[0]}_{user_id}"
         try:
             file = open("./aji_bot/plugins/user_translator/config.ini", "r")
             config = json.loads(file.read())
         except:
             file = open("./aji_bot/plugins/user_translator/config.ini", "w")
-            config = []
+            config = {}
             file.write(json.dumps(config))
         file.close()
         if session_id in config:
-            config.remove(session_id)
+            del config[session_id]
             file = open("./aji_bot/plugins/user_translator/config.ini", "w")
             file.write(json.dumps(config))
-            await matcher.send(f"成功关闭 QQ{cmd} 的发言翻译功能")
+            await matcher.send(f"成功关闭 QQ{user_id} 的发言翻译功能")
             file.close()
-            TRANSLATE_USERS = tuple(config)
-            print(TRANSLATE_USERS)
+            TRANSLATE_USERS = config
         else:
-            await matcher.send(f"QQ{cmd} 的发言翻译功能未开启")
+            await matcher.send(f"QQ{user_id} 的发言翻译功能未开启")
 
 # Event: translate for particular users
 matcher = on_message(temp=False, priority=2, block=True)
@@ -104,9 +112,11 @@ matcher = on_message(temp=False, priority=2, block=True)
 async def translate(event:GroupMessageEvent):
     if event.get_session_id() in TRANSLATE_USERS:
         req.SourceText = event.get_plaintext()
+        req.Source = TRANSLATE_USERS[event.get_session_id()]["source"]
+        req.Target = TRANSLATE_USERS[event.get_session_id()]["target"]
         try:
             resp = client.TextTranslate(req)
-            msg = "【机翻】" + json.loads(resp.to_json_string())['TargetText']
+            msg = "【自動翻訳】" + json.loads(resp.to_json_string())['TargetText']
             await matcher.send(msg)
         except TencentCloudSDKException as err:
             await nonebot.get_bot().send_group_msg(
