bf4060010a8e7f8b661bf96bb080a938d735c803
Shinnenijou <littlesword111@gmail.com>
2022-03-10 03:31:57 +0800

    Update __init__.py

--- src/plugins/helper/__init__.py
+++ src/plugins/helper/__init__.py
@@ -11,6 +11,7 @@ async def help_menu(event:GroupMessageEvent):
     menu = "欢迎使用JiBot鸡器人! 阿鸡爱你哦😘\n现支持以下模块:\n\n"
     menu += '【愿望单监听推送】发送 "愿望单帮助" 获取命令帮助\n'
     menu += '【发言自动翻译】发送 "发言翻译帮助" 获取命令帮助\n'
-    menu += '【推特监听推送】发送 "推特帮助" 获取命令帮助'
+    menu += '【推特监听推送】发送 "推特帮助" 获取命令帮助\n'
+    menu += '【bili监听推送】发送"bili帮助" 获取命令帮助'
     await helper.finish(menu)
     
