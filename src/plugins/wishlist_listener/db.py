9ae4b32a352efa99cfabe20423e3e0c8fa182eea
Shinnenijou <littlesword111@gmail.com>
2022-03-25 15:19:20 +0800

    update
    重写了愿望单插件数据库逻辑

--- src/plugins/wishlist_listener/db.py
+++ src/plugins/wishlist_listener/db.py
@@ -1,15 +1,22 @@
 # -*- coding: utf-8 -*-
+from locale import currency
 import sqlite3
 from sqlite3 import OperationalError
 from os import mkdir
 from sre_constants import SUCCESS
 from typing import Tuple
 
-DATA_PATH = './data'
-WISHLIST_DIR_PATH = f'{DATA_PATH}/wishlist_listener'
-DB_PATH = f'{WISHLIST_DIR_PATH}/wishlists.db'
+from loguru import logger
 
+DATA_PATH = './data'
+WISHLIST_DIR_PATH = f'{DATA_PATH}/wishlist'
+DB_PATH = f'{WISHLIST_DIR_PATH}/wishlist.db'
+
+# 数据库初始化
 def init() -> None:
+    """
+    初始化数据库,如果没有user_list则创建新表
+    """
     try:
         mkdir(DATA_PATH)
     except FileExistsError:
@@ -18,220 +25,204 @@ def init() -> None:
         mkdir(WISHLIST_DIR_PATH)
     except FileExistsError:
         pass
+    _creat_user_list()
+
+def _creat_user_list():
+    """
+    创建用于保存监听对象信息的主表。Amazon愿望单url中的lid为愿望单唯一标识, 主表仅含lid一项
+    """
     connection = sqlite3.connect(DB_PATH)
     cursor = connection.cursor()
-    # 监听信息表(主表)
-    try:
-        cursor = cursor.execute(
-            """
-            CREATE TABLE WISHLISTS (
-                ID INT PRIMARY KEY NOT NULL,
-                GROUP_ID INT NOT NULL,
-                TARGET_NAME VARCHAR(255) NOT NULL,
-                URL VARCHAR(255) NOT NULL
-            );
-            """
-        )
-    except OperationalError:
-        pass
-    count = cursor.execute('SELECT count(*) FROM WISHLISTS;').fetchone()[0]
-    if count == 0:
-        cursor.execute(
-            f"""
-            INSERT INTO WISHLISTS VALUES(0, -1, 'NULL', 'NULL');
-            """.strip()
-        )
-    connection.commit()
+    # 检查主表是否存在
+    table_exist = cursor.execute(
+        'select count(*) from sqlite_master where type="table" and name="user_list";'
+    ).fetchone()[0]
+    # 不存在则创建新表
+    if not table_exist:
+        cursor = cursor.execute('create table user_list(lid varchar(255) primary key not null);')
+        connection.commit()
+    cursor.close()
     connection.close()
-
-def get_users_on(group_id : int = None) -> Tuple[str,...]:
-    with sqlite3.connect(DB_PATH) as connection:
-        cursor = connection.cursor()
-        cursor = cursor.execute(
-            f"""
-            select target_name from wishlists where group_id={group_id};
-            """
-        )
-        ret = list(name[0] for name in cursor.fetchall())
-    return ret
-
-def get_all_users() -> set:
-    with sqlite3.connect(DB_PATH) as connection:
-        cursor = connection.cursor()
-        cursor = cursor.execute(
-            f"""
-            select target_name from wishlists where id!=0;
-            """
-        )
-        ret = set(name[0] for name in cursor.fetchall())
-    return ret
-
-def get_groups_on(name : str = None) -> list[int]:
+    
+# 监听用户管理
+# 主表columns: lid
+def get_user_list() -> list[str]:
     """
-    获取订阅给定名称愿望单的群
+    获取监听对象lid的列表
     """
     with sqlite3.connect(DB_PATH) as connection:
         cursor = connection.cursor()
-        cursor = cursor.execute(
+        data = cursor.execute(
             f"""
-            select group_id from wishlists where target_name='{name}';
+            select lid from wishlists;
             """
-        )
-        ret = list(group[0] for group in cursor.fetchall())
-    return ret
+        ).fetchall()
+        user_list = [row[0] for row in data]
+        cursor.close()
+    return user_list
 
-def get_items(name : str) -> list[str]:
-    with sqlite3.connect(DB_PATH) as connection:
-        cursor = connection.cursor()
-        cursor = cursor.execute(
-            f"""
-            select commodity from {name} where id!=0;
-            """
-        )
-        ret = list(item[0] for item in cursor.fetchall())
-    return ret
-
-def get_url(name : str) -> str:
+def add_user(lid: str) -> bool:
     """
-    注意: 原则上一个名称只能对应一个URL, 重复的将会在add的阶段被阻止
-    可以存在多个名称对应一个URL
+    向user_list添加监听对象(注意: 该函数不用于群订阅操作)
+    :param lid: 愿望单url中的lid, 可以为唯一标识一个Amazon账号开设的愿望单
     """
     with sqlite3.connect(DB_PATH) as connection:
+        success = False
         cursor = connection.cursor()
-        cursor = cursor.execute(
-            f"""
-            select url from wishlists where target_name='{name}';
-            """
-        )
-    return cursor.fetchone()[0]
-
-def add_listen(group_id : int, name : str, url : str) -> bool:
-    success = True
-    with sqlite3.connect(DB_PATH) as connection:
-        cursor = connection.cursor()
-        cursor = cursor.execute(f"select url from wishlists where target_name='{name}';")
-        url_list = list(url[0] for url in cursor.fetchall())
-        if url_list and url not in url_list:
-            success = False
-        else:
-            cursor = cursor.execute(
-                f"""
-                select id from wishlists where group_id={group_id} and target_name='{name}';
-                """
-            )
-            if not cursor.fetchall():
-                cursor = cursor.execute("select max(id) from wishlists;")
-                id = cursor.fetchone()[0] + 1
-                try:
-                    cursor.execute(
-                        f"""
-                        insert into wishlists values({id}, {group_id}, '{name}', '{url}');
-                        """
-                    )
-                    connection.commit()
-                except:
-                    success = False
-            else:
-                success = False
-    _add_table(name)
-    return success
-
-def delete_listen(group_id : int, name : str) -> bool:
-    success = True
-    with sqlite3.connect(DB_PATH) as connection:
-        cursor = connection.cursor()
-        try:
-            cursor = cursor.execute(
-                f"""
-                delete from wishlists where group_id={group_id} and target_name='{name}';
-                """
-            )
-            cursor = cursor.execute(f"select count(*) from wishlists where target_name='{name}';")
-            count = cursor.fetchone()[0]
-            if not count:
-                cursor = cursor.execute(f"drop table {name};")
-            connection.commit()
-        except OperationalError:
-            success = False
-        
-    return success
-
-def delete_group(group_id : int) -> bool:
-    success = True
-    target_list = get_users_on(group_id)
-    for name in target_list:
-        delete_listen(group_id, name)
-    return success
-
-def update_commodities(name : str, new_items : list[str], buyed_items : list[str]) -> bool:
-    success = True
-    with sqlite3.connect(DB_PATH) as connection:
-        try:
-            cursor = connection.cursor()
-            for item in buyed_items:
-                cursor = cursor.execute(
-                    f"""
-                    delete from {name} where commodity='{item}';
-                    """
-                )
-            cursor = cursor.execute(f'select max(id) from {name};')
-            id = cursor.fetchone()[0] + 1
-            for item in new_items:
-                cursor = cursor.execute(
-                    f"""
-                    insert into {name} values({id}, '{item}');
-                    """
-                )
-                id += 1
-            connection.commit()
-        except OperationalError:
-            success = False
-    return success
-
-def _add_table(table_name : str) -> bool:
-    with sqlite3.connect(DB_PATH) as connection:
-        cursor = connection.cursor()
-        success = True
-        try:
-            cursor = cursor.execute(
-                f"""
-                create table {table_name}(
-                    id int primary key not null,
-                    commodity varchar(255) not null
-                );
-                """
-            )
+        # 查看主表中是否已有记录
+        user_exist = cursor.execute(
+            f'select count(*) from user_list where lid="{lid}";').fetchone()[0]
+        # 如果没有记录则插入新条目
+        if not user_exist:
+            cursor.execute(f'insert into user_list values("{lid}");')
+            # 同时创建用于保存订阅信息的表和现有商品的表
             cursor.execute(
-                f"""
-                insert into {table_name} values(0, 'null');
-                """
-            )
-            connection.commit()
-        except OperationalError:
-            success = False
-    return success
-
-def _delete_table(table_name : str) -> bool:
-    with sqlite3.connect(DB_PATH) as connection:
-        cursor = connection.cursor()
-        success = True
-        try:
+                f'create table sub_{lid} (group_id int primary key not null, name varchar(255) not null);')
             cursor.execute(
-                f"""
-                drop table {table_name};
-                """
-            )
-        except OperationalError:
-            success = False
+                f'create table item_{lid} (item_name varchar(255) not null);')
+            connection.commit()
+            success = True
+        cursor.close()
     return success
 
-def _show_table(table_name : str) -> None:
+def delete_user(lid: str) -> bool:
+    """
+    从user_list移除监听对象(注意: 该函数不用于群订阅操作)
+    :param lid: 愿望单url中的lid, 可以为唯一标识一个Amazon账号开设的愿望单
+    """
+    with sqlite3.connect(DB_PATH) as connection:
+        success = False
+        cursor = connection.cursor()
+        # 查看主表中是否已有记录
+        user_exist = cursor.execute(
+            f'select count(*) from user_list where lid="{lid}";').fetchone()[0]
+        # 如果有记录则进行删除
+        if user_exist:
+            cursor.execute(f'delete from user_list where lid="{lid}";')
+            # 同时删除用于保存订阅信息的表和现有商品的表
+            cursor.execute(f'drop table sub_{lid};')
+            cursor.execute(f'drop table item_{lid};')
+            connection.commit()
+            success = True
+        cursor.close()
+    return success
+
+# 群订阅管理
+# 订阅表columns: group_id, name
+def get_group_sub(group_id: int) -> dict[str,str]:
+    """
+    获取某个群订阅的所有用户
+    :return sub_list: 一个包含所有订阅的字典, key为lid, value为该群提交的订阅用户name
+    :param group_id: 群号
+    """
+    with sqlite3.connect(DB_PATH) as connection:
+        sub_list = {}
+        cursor = connection.cursor()
+        data = cursor.execute(f'select lid from user_list;').fetchall()
+        for row in data:
+            lid = row[0]
+            name = cursor.execute(
+                f'select name from sub_{lid} where group_id={group_id};').fetchone()[0]
+            sub_list[lid] = name
+        cursor.close()
+    return sub_list
+
+def get_sub_group(lid: str) -> dict[int, str]:
+    """
+    获取订阅了某个用户的所有群信息
+    :return group_list: 一个包含所有群的字典, key为群号, value为该群提交的订阅用户name
+    :param lid: 愿望单url中的lid, 可以为唯一标识一个Amazon账号开设的愿望单
+    """
+    with sqlite3.connect(DB_PATH) as connection:
+        group_list = {}
+        cursor = connection.cursor()
+        data = cursor.execute(f'select group_id, name from sub_{lid};').fetchall()
+        for row in data:
+            group_id = row[0]
+            name = row[1]
+            group_list[group_id] = name
+        cursor.close()        
+    return group_list
+
+def add_sub(lid: str, group_id: int, name: str) -> bool:
+    """
+    为某个群添加订阅, 如果该用户是第一次被订阅, 则会同时添加主表条目, 创建订阅表与商品表
+    :param lid: 愿望单url中的lid, 可以为唯一标识一个Amazon账号开设的愿望单
+    :param group_id: 群号
+    :param name: 该群提交的订阅用户name, 用于推送时显示
+    """
+    add_user(lid)  # 添加该用户至主表(变相检查用户及相关表是否存在)
+    with sqlite3.connect(DB_PATH) as connection:
+        success = False
+        cursor = connection.cursor()
+        already_sub = cursor.execute(f'select group_id, name from sub_{lid};').fetchone()[0]
+        if not already_sub:
+            cursor.execute(f'insert into sub_{lid} values({group_id}, "{name}");')
+            connection.commit()
+            success = True
+        cursor.close()        
+    return success
+
+def delete_sub(lid: str, group_id: int) -> bool:
+    """
+    为某个群移除订阅, 如果该用户不再有群订阅, 则删除主表条目和订阅表, 商品表
+    :param lid: 愿望单url中的lid, 可以为唯一标识一个Amazon账号开设的愿望单
+    :param group_id: 群号
+    :param name: 该群提交的订阅用户name, 用于推送时显示
+    """
+    add_user(lid)  # 添加该用户至主表(变相检查用户及相关表是否存在)
+    with sqlite3.connect(DB_PATH) as connection:
+        success = False
+        cursor = connection.cursor()
+        already_sub = cursor.execute(f'select group_id, name from sub_{lid};').fetchone()[0]
+        if already_sub:
+            cursor.execute(f'delete from sub_{lid} where group_id={group_id};')
+            connection.commit()
+            success = True
+        # 在删除一条订阅后检查此时订阅表是否为空
+        sub_empty = cursor.execute(f'select count(*) from sub_{lid};').fetchone()[0] == 0    
+        cursor.close()
+    # 订阅表为空则删除该用户订阅表, 商品表, 以及主表记录
+    if sub_empty:
+        delete_user(lid)
+    return success
+
+def delete_group(group_id: int) -> None:
+    """
+    移除某个群的所有订阅
+    :param group_id: 群号
+    """
+    sub_list = get_group_sub(group_id)
+    for lid in sub_list.keys():
+        delete_sub(lid, group_id)
+
+# 商品管理
+# 商品表clolumn: item_name
+def get_items(lid: str) -> list[str]:
+    """
+    获取某个用户愿望单中现存的所有物品
+    :return item_list: 商品名列表
+    :param lid: 愿望单url中的lid, 可以为唯一标识一个Amazon账号开设的愿望单
+    """
     with sqlite3.connect(DB_PATH) as connection:
         cursor = connection.cursor()
-        cursor.execute(
-            f"""
-            select * from {table_name};
-            """
-        )
-        ret = cursor.fetchall()
-    return ret
+        cursor.execute(f'select item_name from item_{lid};')
+        item_list = [row[0] for row in cursor.fetchall()]
+        cursor.close()
+    return item_list
+
+def update_items(lid: str, new_items: list[str], removed_items: list[str]) -> None:
+    """
+    更新商品表中的, 新增商品和移除商品的数据
+    :param lid: 愿望单url中的lid, 可以为唯一标识一个Amazon账号开设的愿望单
+    :param new_items: 新增商品名列表
+    :param removed_items: 被移除商品名列表
+    """
+    with sqlite3.connect(DB_PATH) as connection:
+        cursor = connection.cursor()
+        for item in removed_items:
+            cursor.execute(f'delete from item_{lid} where item_name="{item}";')
+        for item in new_items:
+            cursor = cursor.execute(f'insert into item_{lid} values("{item}");')
+        connection.commit()    
+        cursor.close()
--- src/plugins/wishlist_listener/utils/amzreq.py
+++ src/plugins/wishlist_listener/utils/amzreq.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from cgitb import text
 import aiohttp
 import requests
 import asyncio
@@ -29,12 +30,12 @@ async def _request(session:aiohttp.ClientSession, url:str, headers:dict[str,str]
     获取单一url的愿望单页面
     """
     async with session.get(url=url, headers=headers, proxy=PROXY) as resp:
-        wishlist = ""
         try:
-            wishlist = await resp.text()
+            text = await resp.text()
         except Exception as err:
+            text = ""
             logger.error(f'请求愿望单时发生错误: {err}')
-    return wishlist
+    return text
 
 def _find_one(string:str, begin:int):
     item_beg = string.find("itemName", begin, len(string))
@@ -70,22 +71,23 @@ def check_items(list1 : list, list2 : list):
             not_include.append(item)
     return not_include
 
-def make_notice(new_items : list[str],buyed_items : list[str],name : str,url : str): 
+def make_notice(new_items: list[str], buyed_items: list[str], url: str): 
     """
     通过给定的new_items和buyed_items构造通知信息, 如果两个items都是空则返回空的字符串
     """ 
     msg = ""
     if new_items:
-        msg += f"{name}のほしい物リストに以下の商品が追加されました:\r\n"
+        msg += "--------------------\n"
+        msg += f"以下の商品が追加されました:\n"
         for i in range(len(new_items)):
-            msg += f'[{i + 1}]{new_items[i]}\r\n'
-        msg += "\r\n"
+            msg += f'[{i + 1}]{new_items[i]}\n'
     if buyed_items:
-        msg += f"{name}のほしい物リストに以下の商品が削除されました:\r\n"
+        msg += "--------------------\n"
+        msg += f"以下の商品が削除されました:\n"
         for i in range(len(buyed_items)):
-            msg += f'[{i + 1}]{buyed_items[i]}\r\n'
-        msg += "\r\n"
+            msg += f'[{i + 1}]{buyed_items[i]}\n'
     if msg:
+        msg += "--------------------\n"
         msg += url
     return msg
 
@@ -94,3 +96,9 @@ def is_clear(text : str) -> bool:
     检查请求到的页面是不是真的没有商品。没有商品的页面信息中将会包含相关提示
     """
     return text.find("このリストにはアイテムはありません") >= 0
+
+def lid_to_url(*lid_list: str) -> list[str]:
+    """
+    将用户lid转换为对应的url进行请求
+    """
+    return [f'https://www.amazon.co.jp/hz/wishlist/ls/{lid}' for lid in lid_list]
