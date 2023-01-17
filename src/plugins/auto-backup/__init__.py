feb63b1caa85df9d1eba625a48c832dae2ae5e29
shinnenijou <littlesword111@gmail.com>
2023-01-17 14:03:25 +0800

    Update __init__.py
    备份完成后自动结束进程，systemd完成重启

--- src/plugins/auto-backup/__init__.py
+++ src/plugins/auto-backup/__init__.py
@@ -1,8 +1,9 @@
-from importlib.resources import path
 import nonebot
+import signal
 from nonebot import require, logger
 import time
 import os
+
 
 # 读取备份地址
 BACKUP_PATH = SESSDATA = nonebot.get_driver().config.dict()['backup_path']
@@ -15,7 +16,7 @@ except FileExistsError:
 auto_backup = require('nonebot_plugin_apscheduler').scheduler
 # 每周UTC Sun. 20:00(GTM+8, Mon. 4:00)启动自动备份, 
 @auto_backup.scheduled_job('cron', 
-    day_of_week='thu', hour=5, minute=20, 
+    day_of_week='sun', hour=20, minute=00, 
     timezone='UTC', id='db_backup')
 @logger.catch
 async def backup():
@@ -27,5 +28,9 @@ async def backup():
     except FileExistsError:
         pass
     os.system(f'cp -r data {BACKUP_PATH}/botDB_backup_{date}')
-    for job in auto_backup.get_jobs():
-        job.resume()
+
+    # 中断当前进程, 由systemd完成重启
+    os.kill(os.getpid(), signal.SIGINT)
+
+    #for job in auto_backup.get_jobs():
+    #    job.resume()
