67db09fb1bb1f14d0a24ec209df2870c84a30014
shinnenijou <littlesword111@gmail.com>
2023-11-09 00:43:13 +0800

    modify utils functions

--- src/common/utils.py
+++ src/common/utils.py
@@ -1,11 +1,12 @@
 import os
+from pathlib import Path
 from datetime import datetime
 import pytz
 import asyncio
 
 import nonebot
 from nonebot.adapters.onebot.v11 import GroupMessageEvent, Message, GroupIncreaseNoticeEvent, MessageSegment
-from nonebot.log import logger
+from nonebot import logger
 
 
 class Singleton:
@@ -19,21 +20,24 @@ class Singleton:
 
 def Mkdir(path: str):
     "Create a directary if not exists"
-    try:
-        os.mkdir(path)
-    except FileExistsError:
-        pass
+    if Path.exists(path):
+        return
+    
+    Path.mkdir(path, parents=True)
 
 
 def Touch(path: str, orig_text: str = "") -> bool:
     "Create a new file if not exists"
-    try:
-        file = open(path, 'r')
-        file.close()
-    except FileNotFoundError:
-        file = open(path, 'w')
+    if Path.exists(path):
+        return
+    
+    dir = os.path.dirname(path)
+
+    if not Path.exists(dir):
+        Path.mkdir(dir, parents=True)
+    
+    with open(path, 'w') as file:
         file.write(orig_text)
-        file.close()
 
 
 def get_group_id(event: GroupMessageEvent) -> str:
@@ -57,6 +61,7 @@ def safe_get_bot():
 
     return bot
 
+
 async def get_qq_name(group_id, user_id) -> str:
     bot = safe_get_bot()
 
@@ -71,10 +76,12 @@ async def get_qq_name(group_id, user_id) -> str:
         user_name = user_info['card']
     return user_name
 
+
 def get_datetime(timezone: str = "Asia/Shanghai") -> str:
     tz = pytz.timezone(timezone)
     dt_now = datetime.now(tz)
     return dt_now.strftime("%Y%m%d_%H%M%S")
+
 
 def parse_datetime(timestr: str, timezone: str = "Asia/Shanghai") -> int:
     if len(timestr) < 15:
@@ -89,21 +96,27 @@ def parse_datetime(timestr: str, timezone: str = "Asia/Shanghai") -> int:
 
     return int(dt.timestamp())
 
-async def send_to_admin(message: str):
+
+async def send_to_group(message: Message, group_id: str):
     bot = safe_get_bot()
 
     if bot is None:
         return
+    
+    try:
+        await bot.send_group_msg(
+            group_id=group_id,
+            message=message
+        )
+    except Exception as e:
+        logger.error(f"Send message error: {str(e)}")
+    
 
+async def send_to_admin(message: Message):
     admin_group = nonebot.get_driver().config.dict().get('admin_group', '')
 
     if not admin_group:
         return
 
-    try:
-        await bot.send_group_msg(
-            group_id=admin_group,
-            message=message
-        )
-    except Exception as e:
-        logger.error(f"Send message error: {str(e)}")
+    await send_to_group(message)
+
