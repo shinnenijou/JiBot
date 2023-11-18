ecc5acf287e0c5824e422abedd57ed087df88cfa
Shinnenijou <littlesword111@gmail.com>
2023-11-19 05:41:51 +0800

    modify database filename

--- src/plugins/db/interface.py
+++ src/plugins/db/interface.py
@@ -1,7 +1,7 @@
 import os
 import typing
 
-from sqlalchemy import create_engine, select, update, delete
+from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
 from nonebot import get_driver
@@ -10,7 +10,7 @@ from .define import *
 from .base import Base
 
 
-engine = create_engine(f"sqlite:///{os.path.join(get_driver().config.dict().get('data_dir', 'data'), '_data.db')}")
+engine = create_engine(f"sqlite:///{os.path.join(get_driver().config.dict().get('data_dir', 'data'), 'data.db')}")
 Base.metadata.create_all(engine)
 
 db_proxy = Session(engine)
